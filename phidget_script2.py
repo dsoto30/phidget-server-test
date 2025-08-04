c2d22ffb367b07a5e2920cf39328c9e6e8012dac
Daniel Soto <sotodaniel531@gmail.com>
2025-08-04 14:08:21 -0700

    got phidget server working, must set it through Phidget Control Panel

---
+++ phidget_script2.py
@@ -0,0 +1,34 @@
+from Phidget22.Phidget import *
+from Phidget22.Net import *
+from Phidget22.Devices.RFID import *
+
+def onTag(self, tag, protocol):
+	print("Tag: " + str(tag))
+	print("Protocol: " + RFIDProtocol.getName(protocol) + " from script 2")
+	print("----------")
+
+def onTagLost(self, tag, protocol):
+	print("Tag: " + str(tag))
+	print("Protocol: " + RFIDProtocol.getName(protocol) + " from script 2")
+	print("----------")
+
+def main():
+	Net.addServer("PHIDGET", "localhost", 5661, "", 0);
+
+	rfid0 = RFID()
+
+	rfid0.setIsRemote(True)
+
+	rfid0.setOnTagHandler(onTag)
+	rfid0.setOnTagLostHandler(onTagLost)
+
+	rfid0.openWaitForAttachment(20000)
+
+	try:
+		input("Press Enter to Stop\n")
+	except (Exception, KeyboardInterrupt):
+		pass
+
+	rfid0.close()
+
+main()
